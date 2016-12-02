6859c585e9e30457f0cfa5d24784dbda656821f7
Korek302 <jacek_blady@interia.pl>
2016-12-02 11:55:28 +0100

    2.12

--- 7_2.ml
+++ 7_2.ml
@@ -8,54 +8,40 @@ let rec put = function
 	| (value, Empty) -> Node(value, Empty, Empty)
 ;;
 
-let slownik = Empty;;
-let slownik = put (("mleko","milk"), slownik);;
-let slownik = put (("kot","cat"), slownik);;
-let slownik = put (("pies","dog"), slownik);;
-let slownik = put (("auto","car"), slownik);;
-let slownik = put (("kon","horse"), slownik);;
-let slownik = put (("aparat","camera"), slownik);;
-let slownik = put (("koto","koto"), slownik);;
-
-	
 let rec english(bt, polish) =
 	match bt with
 		Node(v, l, r) -> 	if fst v > polish then english(l, polish)
 					else if fst v < polish then english(r, polish)
 					else snd v
-		|Empty -> failwith "no such word in dictionary"
-;;
+		|Empty -> failwith "no such word in dictionary";;
 
-english(slownik, "kot");;
-english(slownik, "kon");;
-english(slownik, "pies");;
-english(put(("ryba","fish"),slownik), "ryba");;
-let slownik = put(("ryba","fish"),slownik);;
-
-let add() =
-	print_string "Podaj angielskie znaczenie \n";
-	let ang = read_line() in
-	print_string "Podaj polskie znaczenie \n";
-	let pl = read_line() in
-	put((pl, ang), slownik);;
-	
-
-let search =
-	print_string "Czego wyszukac? \n";
-	let value = read_line() in
-	english(slownik, value);;
-
-let interface choice = let choice = read_line() in
-	begin
-	print_string "1. Dodaj do slownika \n";
-	print_string "2. Wyszukaj w slowniku \n";
-	
-	while choice <> "0" do
-		match choice with
-		| "1" -> add()
-		| "2" -> search()
-		
-		choice = read_line();
-		done;
-		print_newline()
-		end;;
+let interface()=
+	let rec temp(dictionary)=
+		print_string("1. Dodaj slowo do slownika\n2. Wyszukaj slowo w slowniku\n");
+		let choice = read_line() in
+		if choice = "1;;" then
+			(
+			print_string("Podaj slowo po polsku\n");
+			let pol = read_line() in print_endline("");
+			print_string("Podaj znaczenie po angielsku\n");
+			let eng = read_line() in print_endline("");
+			let dictionary = put((pol, eng), dictionary) in
+			print_endline("");
+			temp(dictionary)
+			)
+		else if choice = "2;;" then
+			(
+			print_string("Podaj slowo po polsku\n");
+			let pol = read_line() in print_endline("");
+			print_string(english(dictionary, pol));
+			print_endline("");
+			temp(dictionary);
+			print_endline("");
+			)
+		else 
+			(
+			print_string "Zakonczono dzialanie programu "; 
+			print_endline("");
+			)
+		in temp(Empty)
+		;;
